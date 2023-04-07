80e8ad8752717d50b82f1493d9b933191b075b1e
sabyasachirakshit <sabyasachi@121connection.com>
2023-04-07 10:27:20 +0530

    enabling cors

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
+
 const port = 9000;
 
-app.use("/",(req,res)=>{
-    res.json({"message":"Hello from Express app"});
-})
+app.get("/", (req, res) => {
+  console.log("hitting server on / path");
+  res.json({ message: "Hello from Express app" });
+});
 
-app.listen(port,()=>{
-    console.log(`starting server on ${port}`);
-})
+app.get("/test", (req, res) => {
+  console.log("hitting server on /test path");
+  res.json({ message: "Hello from Express test app" });
+});
+
+app.listen(port, () => {
+  console.log(`starting server on ${port}`);
+});
