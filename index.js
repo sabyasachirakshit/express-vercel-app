e581d210d80d8550809e24e059fdcc571deccbac
sabyasachirakshit <sabyasachi@121connection.com>
2023-04-07 10:33:00 +0530

    updated urls

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ app.use(cors());
 
 const port = 9000;
 
-app.get("/", (req, res) => {
+app.get("/api/", (req, res) => {
   console.log("hitting server on / path");
   res.json({ message: "Hello from Express app" });
 });
 
-app.get("/test", (req, res) => {
+app.get("/api/test", (req, res) => {
   console.log("hitting server on /test path");
   res.json({ message: "Hello from Express test app" });
 });
